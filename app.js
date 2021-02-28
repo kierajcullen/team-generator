67c8e527c0dc1857a37d0f845633b1fccf8d6b29
kierajcullen <kierajcullen@gmail.com>
2021-02-27 19:45:25 -0500

    lots of changes and a working program

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
+const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
 // path can join paths together, similar to fs
 // use the output path.. maybe. play around with this
 // write data using render to teamMembers array, call
@@ -14,6 +16,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 var teamMembers = [];
+var employees = [];
 
 const managerQuestions = [
   {
@@ -26,6 +29,11 @@ const managerQuestions = [
       }
       return true;
     },
+  },
+  {
+    type: "input",
+    message: "Please enter your unique ID: ",
+    name: "ID",
   },
   {
     type: "input",
@@ -44,7 +52,7 @@ const managerQuestions = [
     message: "Please enter the office number: ",
     validate: function (input) {
       if (isNaN(input)) {
-        return "Please enter a number";
+        return "Please enter a valid number. Leave out the () or -.";
       }
       return true;
     },
@@ -52,7 +60,7 @@ const managerQuestions = [
   {
     type: "list",
     name: "totalTeam",
-    message: "Do you have any team members?",
+    message: "Do you have team members to add?",
     choices: ["Yes", "No"],
   },
 ];
@@ -64,10 +72,15 @@ const employeeQuestions = [
     message: "Please enter the employee name: ",
     validate: function (input) {
       if (input == "") {
-        return "Please enter a name.";
+        return "Please enter a real name.";
       }
       return true;
     },
+  },
+  {
+    type: "input",
+    message: "Please enter your unique ID: ",
+    name: "ID",
   },
   {
     type: "input",
@@ -92,72 +105,96 @@ const employeeQuestions = [
     },
     type: "input",
     name: "github",
-    message: "Engineer, enter your github username:",
+    message: "Please enter your GitHub username: ",
     validate: function (input) {
       if (input.trim() == "") {
-        return "Please enter a valid GitHub username";
+        return "Please enter a valid GitHub username.";
       }
       return true;
     },
   },
-  // add a question to add more people and createTeamList must update, call function again
+  {
+    when: (input) => {
+      return input.role == "intern";
+    },
+    type: "input",
+    name: "school",
+    message: "Please enter your school: ",
+    validate: function (input) {
+      if (input.trim() == "") {
+        return "Please enter a valid school.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "totalTeam",
+    message: "Do you have team members to add?",
+    choices: ["Yes", "No"],
+  },
 ];
 
 function createTeamList() {
   inquirer.prompt(employeeQuestions).then((employeeInfo) => {
-    console.log(employeeInfo);
+    // buildHtmlPage(outputPath, render(employees));
+    if (employeeInfo.role == "intern") {
+      let newIntern = new Intern(
+        employeeInfo.name,
+        employeeInfo.ID,
+        employeeInfo.email,
+        employeeInfo.school
+      );
+      teamMembers.push(newIntern);
+    } else if (employeeInfo.role == "engineer") {
+      let newEngineer = new Engineer(
+        employeeInfo.name,
+        employeeInfo.ID,
+        employeeInfo.email,
+        employeeInfo.github
+      );
+      teamMembers.push(newEngineer);
+    } else {
+      buildHtmlPage(outputPath, render(teamMembers));
+    }
+    if (employeeInfo.totalTeam === "Yes") {
+      createTeamList();
+    }
   });
 }
 
 // use the fs write to file and call in the
 
-function buildHtmlPage() {
-  fs.writeFileSync(outputPath, render(teamMembers), function (err) {
+function buildHtmlPage(fileName, data) {
+  fs.writeFileSync(fileName, data, function (err) {
     if (err) {
       console.log(err);
     } else {
-      console.log("success");
+      console.log("Success. Your team log was created.");
     }
   });
+  console.log(teamMembers);
 }
 
 function init() {
   inquirer.prompt(managerQuestions).then((managerInfo) => {
     // const manager = new Manager
-    const manager = managerInfo.name;
+    const manager = new Manager(
+      managerInfo.name,
+      managerInfo.ID,
+      managerInfo.email,
+      managerInfo.officeNumber
+    );
     // pass in the parameters created in the manager.js
     // add the info to the team array
     teamMembers.push(manager);
     //  == means loosely equals
-    if (managerInfo.totalTeam == "yes") {
+    if (managerInfo.totalTeam == "Yes") {
       createTeamList();
     } else {
-      buildHtmlPage();
+      buildHtmlPage(outputPath, render(teamMembers));
     }
   });
 }
 
 init();
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
